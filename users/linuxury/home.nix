8b729776e3ec3eb21753ff93516e192d1aec9623
Linuxury <linuxurypr@gmail.com>
2026-05-16 19:30:45 -0400

    noctalia-color-sync: sync MangoWC focuscolor with Noctalia accent on wallpaper change

--- users/linuxury/home.nix
+++ users/linuxury/home.nix
@@ -29,6 +29,7 @@
   imports = [
     ../../modules/home/neovim.nix
     ../../modules/services/hypr-matugen.nix
+    ../../modules/services/noctalia-color-sync.nix
   ];
 
   # =========================================================================
