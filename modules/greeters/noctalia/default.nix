ca75f077c02350302190e2284cb0b2bebb0b6708
Linuxury <linuxurypr@gmail.com>
2026-08-15 08:51:59 -0400

    Fix pkgs.system deprecation warning, bump home-manager pin
    pkgs.system -> pkgs.stdenv.hostPlatform.system in noctalia greeter module.
    home-manager bump (2026-08-06 -> 2026-08-14) incidentally resolved the
    stdenv.isLinux eval warning too. Remaining stdenv.isDarwin warning traces
    to an unreferenced transitive input, not fixable here.

--- modules/greeters/noctalia/default.nix
+++ modules/greeters/noctalia/default.nix
@@ -41,7 +41,7 @@
 
   programs.noctalia-greeter = {
     enable = true;
-    package = inputs.noctalia-greeter.packages.${pkgs.system}.default;
+    package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
     settings = {
       session.default = "Hyprland (quiet)";
